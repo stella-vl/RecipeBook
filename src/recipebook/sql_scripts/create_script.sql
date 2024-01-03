d100101713be6a68e5af22e82cb264b75d6b4aaa
stella-vl <stella6.vl@gmail.com>
2024-01-03 08:51:15 +0200

    adding

--- src/recipebook/sql_scripts/create_script.sql
+++
@@ -1,9 +0,0 @@
-create database recipebbok;
-
-create table users (
-id int not null primary key auto_increment,
-username varchar(32),
-password varchar(32),
-created timestamp,
-is_active boolean
-);
--- src/recipebook/sql_scripts/insert_script.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `users`(`username`, `password`, `is_active`) VALUES ('katerina','cGFzc3dvcmQxMjM0NTY=', 1);
-INSERT INTO `users`(`username`, `password`, `is_active`) VALUES ('monika','cGFzc3dvcmQxMjM0NTY=', 1);
-INSERT INTO `users`(`username`, `password`, `is_active`) VALUES ('stella','cGFzc3dvcmQxMjM0NTY=', 1);
